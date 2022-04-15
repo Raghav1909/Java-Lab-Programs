9752c9b9678203e64364ae0b3c21b757237711b2
Pranav K Bhaskar <pkb19032002@gmail.com>
2022-04-15 13:27:43 +0530

    Files added

--- Program3.java
+++ Program3.java
@@ -1,21 +1,56 @@
-class Point{
-    int x1,,y1,x2,y2,x3,y3;
-    
+import javax.lang.model.util.ElementScanner14;
 
-    void createPoints(){
-        Scanner ip = new Scanner(System.in);
-        System.out.println("Enter three points:");
-        x1 = ip.nextInt();
-        y1 = ip.nextInt();
-        x2 = ip.nextInt();
-        y2 = ip.nextInt();
-        x3 = ip.nextInt();
-        y3 = ip.nextInt();
+class Point {
+    int x, y;
 
-
+    Point() {
+        x = 0;
+        y = 0;
+        System.out.println("Point created at (" + x + "," + y + ")");
     }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+        System.out.println("Point created at (" + x + "," + y + ")");
+        quadrant();
+    }
+
+    void quadrant() {
+        if (x > 0 && y > 0)
+            System.out.println("Point lies in first quadrant");
+        else if (x < 0 && y > 0)
+            System.out.println("Point lies in second quadrant");
+        else if (x < 0 && y < 0)
+            System.out.println("Point lies in third quadrant");
+        else
+            System.out.println("Point lies in fourth quadrant");
+    }
+
+    void checkTriangle(Point a, Point b, Point c) {
+        if ((a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) == 0)
+            System.out.println("Does not form triangle");
+
+        else
+            System.out.println("Forms triangle");
+    }
+
+    void distance(Point a, Point b) {
+        double dist;
+        dist = Math.sqrt((b.x - a.x) * (b.x - a.x) - (b.y - a.y) * (b.y - a.y));
+        System.out
+                .println("Distance between points (" + a.x + "," + a.y + ") and (" + b.x + "," + b.y + ") is " + dist);
+    }
+
 }
 
 public class Program3 {
-
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        Point p2 = new Point(3, 2);
+        Point p3 = new Point(-4, -5);
+        Point res = new Point();
+        res.checkTriangle(p1, p2, p3);
+        res.distance(p1, p2);
+    }
 }
