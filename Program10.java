b870c2d0b6d330733d8fa2150970b68e7fa88736
Pranav K Bhaskar <pkb19032002@gmail.com>
2022-04-19 17:56:40 +0530

    Changes made

--- Program10.java
+++ Program10.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
+class Sort<T> {
+    int n;
+    T[] arr = new T[n];
+
+    void read() {
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Enter the size of arrays");
+        n = ip.nextInt();
+        System.out.println("Enter the array elements");
+        for (int i = 0; i < n; i++)
+            arr[i] = ip.nextInt();
+    }
+}
+
 public class Program10 {
     public static void main(String[] args) {
 
